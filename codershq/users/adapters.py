fad08303a34c0fcc39a365e73cf453053772be26
Musab <musab.obaid@gmail.com>
2021-10-05 14:49:01 +0400

    Social login adaptor updated

--- codershq/users/adapters.py
+++ codershq/users/adapters.py
@@ -3,8 +3,11 @@ from typing import Any
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.conf import settings
-from django.http import HttpRequest
-
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth import get_user_model
+from allauth.exceptions import ImmediateHttpResponse
+from django.contrib import messages
+from django.shortcuts import redirect
 
 class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest):
@@ -12,5 +15,30 @@ class AccountAdapter(DefaultAccountAdapter):
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
+
+    def pre_social_login(self, request, sociallogin):
+
+        
+        try:
+            get_user_model().objects.get(email=sociallogin.user.email)
+        except get_user_model().DoesNotExist:
+            messages.error(request, 'Your GitHub email is not associated with any existing user. Please Sign up firt!')
+            # raise ImmediateHttpResponse(HttpResponse(status=500))
+            #abort the login
+            raise ImmediateHttpResponse(redirect('/accounts/login'))
+        else:
+            user = get_user_model().objects.get(email=sociallogin.user.email)
+            if not sociallogin.is_existing:
+                sociallogin.connect(request, user) 
+
     def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
+
+
+
+
+
+#useful links:
+#https://github.com/pennersr/django-allauth/issues/418
+# https://fluffycloudsandlines.blog/using-django-allauth-for-google-login-to-any-django-app/
+#https://ilovedjango.com/django/authentication/allauth/allauth-django/
--- config/settings/base.py
+++ config/settings/base.py
@@ -280,7 +280,7 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 # ------------------------------------------------------------------------------
 ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
@@ -289,6 +289,8 @@ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_ADAPTER = "codershq.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 SOCIALACCOUNT_ADAPTER = "codershq.users.adapters.SocialAccountAdapter"
+SOCIALACCOUNT_EMAIL_REQUIRED = True 
+
 # django-compressor
 # ------------------------------------------------------------------------------
 # https://django-compressor.readthedocs.io/en/latest/quickstart/#installation
